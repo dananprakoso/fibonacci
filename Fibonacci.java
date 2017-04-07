85c6c202c40b9f1eda09e3bd10427883c7a2fd00
dananprakoso <dananprakoso10@gmail.com>
2017-04-07 10:14:18 +0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,22 +1,33 @@
-package fibonacci;
-import java.util.Scanner;
-public class Fibonacci {
-    public static void main(String[]args) {
-        Scanner show = new Scanner(System.in);
-        System.out.print("Masukan Jumlah Deret Fibonacci: ");
-        int n = show.nextInt();
-        long fib[] = new long[n];
-         
-        fib[0] = 1;
-        fib[1] = 1;
-         
-        for(int i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
-        }
-         
-        for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
-        }
-    }
- 
+public class belah_ketupat {
+
+  public static void main(String[] args) {
+
+
+    for (int i=1;i<=4;i++){
+             for(int j=4;j>=i;j--){
+                 System.out.print(" ");
+             }
+             for(int k=1;k<=i;k++){
+                 System.out.print("*");
+             }
+             for (int l=1;l<=i-1;l++){
+                 System.out.print("*");
+             }
+             System.out.println();
+         }
+   for (int i=1;i<=5;i++){
+             for(int j=1;j<=i-1;j++){
+                 System.out.print(" ");
+             }
+             for(int z=5;z>=i;z--){
+              System.out.print("*");
+              }
+             for(int k=4;k>=i;k--){
+               System.out.print("*");
+               }
+             System.out.println("");
+         }
+
+  }
+
 }
